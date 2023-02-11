8a45a30a17f16a1999d71e4359ce9577d8f13fa6
Rin <97kostada@gmail.com>
2023-02-11 23:42:19 +0300

    Tests completed

--- src/test/java/PosterTest.java
+++ src/test/java/PosterTest.java
@@ -32,4 +32,49 @@ public class PosterTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void tenLastMovies() {
+        PosterManager manager = new PosterManager(10);
+
+        manager.add("Movie 1");
+        manager.add("Movie 2");
+        manager.add("Movie 3");
+        manager.add("Movie 4");
+        manager.add("Movie 5");
+        manager.add("Movie 6");
+        manager.add("Movie 7");
+        manager.add("Movie 8");
+        manager.add("Movie 9");
+        manager.add("Movie 10");
+
+
+        String[] expected = {"Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void moreLastMovies() {
+        PosterManager manager = new PosterManager(10);
+
+        manager.add("Movie 1");
+        manager.add("Movie 2");
+        manager.add("Movie 3");
+        manager.add("Movie 4");
+        manager.add("Movie 5");
+        manager.add("Movie 6");
+        manager.add("Movie 7");
+        manager.add("Movie 8");
+        manager.add("Movie 9");
+        manager.add("Movie 10");
+        manager.add("Movie 11");
+
+
+        String[] expected = {"Movie 11", "Movie 10", "Movie 9", "Movie 8", "Movie 7", "Movie 6", "Movie 5", "Movie 4", "Movie 3", "Movie 2"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
